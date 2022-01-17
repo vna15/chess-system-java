9a8f571ccfe53942c2254302742afab128b1a186
vna15 <vnazevedo08@hotmail.com>
2022-01-16 23:18:42 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int collumns) {
+		if(rows < 1 || collumns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 collumn");
+		}
 		this.rows = rows;
 		this.collumns = collumns;
 		pieces = new Piece[rows][collumns];
@@ -16,28 +19,44 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getCollumns() {
 		return collumns;
 	}
-
-	public void setCollumns(int collumns) {
-		this.collumns = collumns;
-	}
 	
 	public Piece piece(int row, int collumn) {
+		if(!positionExists(row, collumn)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][collumn];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getCollumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getCollumn()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int collumn) {
+		return row >= 0 && row < rows && collumn >= 0 && collumn < collumns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getCollumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
 	}
 }
